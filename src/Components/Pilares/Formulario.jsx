6a4ac438a1d8b823e091033d6e76037dc6d0cf2d
Pino Luis <pinotorres.luis@gmail.com>
2022-10-18 16:19:33 -0300

    se agrego nuevo formulario

--- src/Components/Pilares/Formulario.jsx
+++ src/Components/Pilares/Formulario.jsx
@@ -9,7 +9,7 @@ function Formulario (props){
         <Modal className="container_body" {...props}>
         <Modal.Header closeButton/>
         <Modal.Body>
-        <iframe className="iframe_container" src="https://docs.google.com/forms/d/e/1FAIpQLSdp9iGuQNsjWAv06YCzcOnhSQN5CqM38WmM-TgMW09Y9nh8KQ/viewform?embedded=true" width="640" height="1233" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+        <iframe className="iframe_container" src="https://docs.google.com/forms/d/e/1FAIpQLSdRbyCht7fRv-7TFhGyefQ5meAx4MGtoUqY0skR8cK_kCdHDQ/viewform?embedded=true" width="640" height="2227" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </Modal.Body>
         </Modal>
         </>
