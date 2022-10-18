e3f6bbdaa2b4c62beaa003630ef50e14c7745eca
Pino Luis <pinotorres.luis@gmail.com>
2022-10-18 16:24:17 -0300

    se corrigio texto

--- src/Components/Intercambiabilidad/CardSocios.jsx
+++ src/Components/Intercambiabilidad/CardSocios.jsx
@@ -36,7 +36,7 @@ const CardSocios = () => {
         <p>
           <FormattedMessage
           id="app.about_texto2"
-          defaultMessage= "IT Work Es una startup creada por 3 amigos y excompañeros de Microsoft que luego de 25 años de desarrollo profesional, cada uno dentro de su especialidad (marketing y ventas, tecnología y management), vieron que muchos de los problemas a los que se enfrentaron durante tantos años son los mismos a los que se enfrentan a diario cientos de empresas, comercios y autónomos, y creen que uniendo esfuerzos y conocimientos pueden hacer que estas empresas se eviten los problemas relacionados a la baja productividad, la escasa información para tomar buenas decisiones, la falta de clientes o de vínculo con los mismos, y el desconocimiento de la tecnología disponible para lograr el éxito de sus negocios en la era digital. Cada uno de los socios de IT work estuvo en esa situación, entiende el costo de no resolver esos problemas y los beneficios de resolverlos, por eso pudieron alcanzar sus objetivos, y están dispuestos a hacer lo mismo por su negocio."
+          defaultMessage= "Cada uno de los socios de IT work estuvo en esa situación, entiende el costo de no resolver esos problemas y los beneficios de resolverlos, por eso pudieron alcanzar sus objetivos, y están dispuestos a hacer lo mismo por su negocio."
           />
         </p>
         </div>
